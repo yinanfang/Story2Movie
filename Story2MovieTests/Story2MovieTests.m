6713ab9a9671ddbd570bb252800494e4f29e133f
yinanfang <yinan_fang@hotmail.com>
2014-08-01 23:06:48 -0400

    Initial Commit

---
+++ Story2MovieTests/Story2MovieTests.m
@@ -0,0 +1,34 @@
+//
+//  Story2MovieTests.m
+//  Story2MovieTests
+//
+//  Created by Golden Compass on 8/1/14.
+//  Copyright (c) 2014 Golden Compass. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Story2MovieTests : XCTestCase
+
+@end
+
+@implementation Story2MovieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
